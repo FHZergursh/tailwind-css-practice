72209333f738df46c58f0268c9be27c5354c63ed
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-02-03 16:58:47 +0000

    simple navbar
    title

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-slate-300 min-h-screen text-slate-900'> {/* Wraps around the whole page, gives a background. Min-h is needed to fit whole page*/}
+      <div className='flex items-center justify-center h-12 bg-blue-500 text-white'>  {/* items-center does vertical, justify center does horizontal */}
+        <div>Logo</div>
+        <div>
+          <span>Home</span>
+          <span>About</span>
+          <span>Contract</span>
+        </div>
 
-
+      </div>
     </div>
   )
 }
