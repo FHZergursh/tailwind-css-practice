e3ce4bca05dac0db4aa9089de8d40d152db8637d
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-02-03 17:19:55 +0000

    center navbar
    title

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-slate-300 min-h-screen text-slate-900'> {/* Wraps around the whole page, gives a background. Min-h is needed to fit whole page*/}
-      <div className='flex items-center justify-center h-12 bg-blue-500 text-white'>  {/* items-center does vertical, justify center does horizontal */}
-        <div>Logo</div>
-        <div>
+      <div className='flex items-center justify-between h-12 bg-blue-500 text-white px-4'>  {/* items-center does vertical, justify center does horizontal */}
+        <div className='font-bold'>Logo</div>
+        <div className='flex gap-2 mx-auto'> {/*mx auto puts them in center of screen */}
           <span>Home</span>
           <span>About</span>
           <span>Contract</span>
